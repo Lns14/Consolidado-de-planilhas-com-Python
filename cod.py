cf7f1a728cf097522c6d2b1e636f1b6f370e8649
Lns14 <livia.novais.sousa@gmail.com>
2024-01-19 17:03:23 -0300

    sistema de verificação da area atingida

--- cod.py
+++ cod.py
@@ -13,5 +13,7 @@ for nome_arquivo in arquivos:
     tabela_vendas = pd.read_csv(os.path.join(caminho, nome_arquivo)) #lê arquivo por arquivo que está em csv econcatena o nome do caminho com o nome do arquivo
     tabela_vendas["Data de Venda"] = pd.to_datetime ("01/01/1900") + pd.to_timedelta (tabela_vendas["Data de Venda"], unit="D") #pega a data inicio considerada pelo excel e soma a quantidade informada na coluna Data de Venda para descobrir a data no formado normal
     tabela_consolidada = pd.concat ([tabela_consolidada, tabela_vendas])
-print (tabela_consolidada)
+tabela_consolidada = tabela_consolidada.sort_values (by = "Data de Venda")
+tabela_consolidada = tabela_consolidada.reset_index (drop = True)
+tabela_consolidada.to_excel ("Vendas.xlsx", index= False)
 
